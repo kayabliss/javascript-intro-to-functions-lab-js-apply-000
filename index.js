09f77bdaaf37e99ac40a7a2700550faa71813185
Ana Pichardo <kayablissfilms@gmail.com>
2018-12-17 02:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function logWhisper(string){
 function sayHiToGrandma (string){
   if(string.toLowerCase() === string )
   return "I can't hear you!"
-  }if(string.toUpperCase() === string){
-  return "YES INDEED!"
-  }else{
+  }
+  if(string.toUpperCase() === string){
+   return "YES INDEED!"
+    }else{
   return "I love you, too"
 }
 
